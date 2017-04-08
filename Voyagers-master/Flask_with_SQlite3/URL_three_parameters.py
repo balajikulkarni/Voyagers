47223e02af03fee695db26d8c84536a9239bdf6e
Balaji Kulkarni <balaji.kulkarni92@gmail.com>
2017-04-08 12:01:27 +0530

    Update URL_three_parameters.py

--- Voyagers-master/Flask_with_SQlite3/URL_three_parameters.py
+++ Voyagers-master/Flask_with_SQlite3/URL_three_parameters.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 app.debug = True
 
 sqlite_file = 'PA_GPRS.db'  #DB Name
-table = 'Arduino_GPRS'      #Table Name
-field = 'GPRS_Data'         #column_name
+table = 'Coordinates'      #Table Name
+field = 'NodeName'         #column_name
 latitude  = 'Latitude'      #column_name
 longitude = 'Longitude'
 field_type = 'TEXT'         #Type
@@ -17,27 +17,30 @@ coord_type = 'REAL'         #Type
 def home():
     return 'Hello Voyagers'
 
-@app.route('/gprsdata')
+@app.route('/senddata')
 def get_gprsdata():
     #if we are here it means we got a POST on /gprsdata
     name = request.args.get('node')
     lat = request.args.get('lat')
     lng= request.args.get('lng')
+    data = (name,lat,lng)
     con = sql.connect(sqlite_file)
+    
     try:
         with con:
             c = con.cursor()
-            c.execute('CREATE TABLE IF NOT EXISTS {tn}({nf} {ft},{nf1} {ft1},{nf2} {ft2})'\
+            c.execute('CREATE TABLE IF NOT EXISTS {tn}({nf} {ft} NOT NULL,{nf1} {ft1} NOT NULL,{nf2} {ft2} NOT NULL)'\
                       .format(tn=table, nf=field, ft=field_type,
                               nf1=latitude, ft1=coord_type,
                               nf2=longitude, ft2=coord_type))
-
-            c.execute('INSERT INTO {} VALUES (?,?,?)'.format(table),(name,lat,lng))
+                  
+            c.execute('INSERT INTO {} VALUES (?,?,?)'.format(table),(data))
             con.commit()
             c.close()
         con.close()
 
         return render_template("main_page.html")
-
-    except Exception:
-        return 'Error'
+        
+    except Exception:    
+        return 'Error Inserting into Database'
+        
